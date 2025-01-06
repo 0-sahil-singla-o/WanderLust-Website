d2d18a4ed10483fae319e8d8b63f182baac1a328
0-sahil-singla-o <163234804+0-sahil-singla-o@users.noreply.github.com>
2025-01-06 12:38:05 +0530

    updated dist

--- Frontend/WanderLust_Frontend/vite.config.js
+++ Frontend/WanderLust_Frontend/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// import {staticCopy} from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/static.json', // Path to your static.json file
+          dest: '.',              // This will copy it to the root of the dist folder
+        }
+      ]
+    })
+  ],
 })
